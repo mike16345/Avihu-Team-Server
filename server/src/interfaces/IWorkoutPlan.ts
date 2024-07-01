d8f2fd0b6a4eb1ffbb62b704a7455a768d861678
mikeg16345 <michaelgani815@gmail.com>
2024-07-01 20:17:29 +0300

    created model for workout plan and schema validations

--- server/src/interfaces/IWorkoutPlan.ts
+++ server/src/interfaces/IWorkoutPlan.ts
@@ -1,3 +1,24 @@
-export interface IWorkoutPlan{
-    
+import { MuscleGroups } from "../enums/MuscleGroups";
+
+export interface ISet {
+  minReps: number;
+  maxReps: number;
+}
+
+export interface IMuscleGroupWorkoutPlan {
+  muscleGroup: string;
+  workouts: IWorkout[];
+}
+
+export interface IWorkout {
+  tipFromTrainer?: string;
+  linkToVideo?: string;
+  name: string;
+  sets: ISet[];
+}
+
+export interface IDetailedWorkoutPlan {
+  planName: string;
+  userId: string;
+  workouts: IMuscleGroupWorkoutPlan[];
 }
---
+++ server/src/models/workoutPlanModel.ts
@@ -0,0 +1,91 @@
+import { Schema, model } from "mongoose";
+import {
+  IMuscleGroupWorkoutPlan,
+  IDetailedWorkoutPlan,
+  IWorkout,
+  ISet,
+} from "../interfaces/IWorkoutPlan";
+import Joi from "joi";
+
+export const setSchema: Schema<ISet> = new Schema({
+  minReps: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  maxReps: {
+    type: Number,
+    required: false,
+    min: 1,
+  },
+});
+
+export const workoutSchema: Schema<IWorkout> = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  sets: {
+    type: [setSchema],
+    required: true,
+  },
+  linkToVideo: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  tipFromTrainer: {
+    type: String,
+    required: false,
+    default: "",
+  },
+});
+
+export const muscleGroupWorkoutPlanSchema: Schema<IMuscleGroupWorkoutPlan> = new Schema({
+  muscleGroup: {
+    type: String,
+    required: true,
+  },
+  workouts: {
+    type: [workoutSchema],
+    required: true,
+  },
+});
+
+const workoutPlanSchema: Schema<IDetailedWorkoutPlan> = new Schema({
+  planName: {
+    type: String,
+    required: true,
+  },
+  userId: { type: String, required: true },
+
+  workouts: {
+    type: [muscleGroupWorkoutPlanSchema],
+    required: true,
+  },
+});
+
+export const WorkoutPlan = model<IDetailedWorkoutPlan>("workoutPlans", workoutPlanSchema);
+
+const setValidationSchema = Joi.object({
+  minReps: Joi.number().min(1).required(),
+  maxReps: Joi.number().min(1).optional(),
+});
+
+const workoutValidationSchema = Joi.object({
+  name: Joi.string().required(),
+  sets: Joi.array().items(setValidationSchema).required(),
+  linkToVideo: Joi.string().optional(),
+  tipFromTrainer: Joi.string().optional(),
+});
+
+const muscleGroupWorkoutPlanValidationSchema = Joi.object({
+  muscleGroup: Joi.string().required(),
+  workouts: Joi.array().items(workoutValidationSchema).required(),
+});
+
+export const WorkoutPlanSchemaValidation = Joi.object({
+  userId: Joi.string().required(),
+  planName: Joi.string().min(1).max(25).required(),
+  workouts: Joi.array().items(muscleGroupWorkoutPlanValidationSchema).required(),
+});
