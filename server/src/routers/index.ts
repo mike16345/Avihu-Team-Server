a2137785ddd29a8622e7ddc4d48ebe41821578c4
mikeg16345 <michaelgani815@gmail.com>
2024-07-19 14:59:06 +0300

    updated router index file

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -3,6 +3,8 @@ import userRouter from "./users";
 import weighInsRouter from "./weighIns";
 import workoutPlanRouter from "./workoutPlans";
 import workoutPlanPresetRouter from "./workoutPlanPresets";
+import recordedSetsRouter from "./recordedSets";
+import dietPlansRouter from "./dietPlans";
 
 const router = express.Router();
 
@@ -10,5 +12,7 @@ router.use("/users", userRouter);
 router.use("/weighIns", weighInsRouter);
 router.use("/workoutPlans", workoutPlanRouter);
 router.use("/workoutPlansPresets", workoutPlanPresetRouter);
+router.use("/recordedSets", recordedSetsRouter);
+router.use("/dietPlans", dietPlansRouter);
 
 export default router;
