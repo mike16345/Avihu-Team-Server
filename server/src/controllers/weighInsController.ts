1389b72f1a497ef3fa8aae0639fc8ecdf9f98fda
mikeg16345 <michaelgani815@gmail.com>
2024-07-26 16:52:11 +0300

    refactored weigh ins API

--- server/src/controllers/weighInsController.ts
+++ server/src/controllers/weighInsController.ts
@@ -1,27 +1,39 @@
 import { Request, Response } from "express";
 import { WeighInSchemaValidation } from "../models/weighInModel";
 import { weighInServices } from "../services/weighInService";
+import { IWeighIn } from "../interfaces/IWeighIns";
+import { UpdateResult } from "mongodb";
 
 class WeighInsController {
   addWeighIn = async (req: Request, res: Response) => {
     const id = req.params.id;
-    const data = {
-      userId: id,
-      weight: req.body.weight,
-    };
+    const weighInToAdd = req.body;
 
-    const { error, value } = WeighInSchemaValidation.validate(data);
+    const { error, value } = WeighInSchemaValidation.validate(weighInToAdd);
 
     if (error) {
       return res.status(400).json({ message: error.message });
     }
 
     try {
-      const weighIn = await weighInServices.addWeighIn(value);
+      const weighIn = await weighInServices.addWeighIn(weighInToAdd, id);
 
       res.status(201).json(weighIn);
     } catch (err) {
       res.status(500).json({ message: "An error occurred while adding the weigh-in." });
+    }
+  };
+
+  addManyWeighIns = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const weighIns = req.body.weighIns as IWeighIn[];
+
+    try {
+      const result = await weighInServices.addManyWeighIns(weighIns, id);
+
+      res.status(201).send(result);
+    } catch (err: any) {
+      res.status(500).json({ message: err.message });
     }
   };
 
@@ -33,22 +45,38 @@ class WeighInsController {
     console.log("weight", weight);
 
     try {
-      const updatedWeighIn = await weighInServices.updateWeighIn(id, weight);
-      console.log("updated ", updatedWeighIn);
-      return res.status(201).json(updatedWeighIn);
+      const updatedWeighIn = (await weighInServices.updateWeighIn(id, weight)) as UpdateResult;
+
+      if (updatedWeighIn.matchedCount == 0) {
+        res.status(404).send({ message: "Weigh in not found!" });
+      }
+
+      res.status(201).json(updatedWeighIn);
     } catch (err) {
-      return res.status(500).json({ message: err });
+      res.status(500).send({ message: err });
     }
   };
 
-  deleteWeighIns = async (req: Request, res: Response) => {
+  deleteUserWeighIns = async (req: Request, res: Response) => {
     try {
       const id = req.params.id;
-      const response = await weighInServices.deleteWeighIns(id);
+      const response = await weighInServices.deleteUserWeighIns(id);
 
       return res.status(201).json(response);
     } catch (err) {
       return res.status(500).json({ message: "There was an error deleting weigh ins." });
+    }
+  };
+
+  deleteWeighInById = async (req: Request, res: Response) => {
+    const { weighInId } = req.params;
+
+    try {
+      const response = await weighInServices.deleteWeighInById(weighInId);
+
+      res.status(200).send(response);
+    } catch (err: any) {
+      return res.status(500).json({ message: err.message });
     }
   };
 
@@ -57,6 +85,10 @@ class WeighInsController {
 
     try {
       const weighIns = await weighInServices.getWeighInsByUserId(id as string);
+
+      if (!weighIns) {
+        return res.status(404).json({ message: "No weigh ins found for this user." });
+      }
 
       return res.status(201).json(weighIns);
     } catch (err) {
