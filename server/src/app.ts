1a5fc1b735258ed622920998e353bc2a875e4264
mikeg16345 <michaelgani815@gmail.com>
2024-07-27 21:22:21 +0300

    fixed issue with merge override

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,40 +1,36 @@
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import userRouter from "./routers/users";
-import weighInsRouter from "./routers/weighIns";
-import workoutPlanRouter from "./routers/workoutPlans";
-import exercisePresetRouter from "./routers/exercisePresets";
-import workoutPlanPresetRouter from "./routers/workoutPlanPresets";
-import muscleGroupPresetsRouter from "./routers/muscleGroupPresets";
-
-
-
 import "./db/mainConnection";
 import router from "./routers";
 import http from "http";
 
 dotenv.config();
-const port = process.env.SERVER_PORT;
 
+const PORT = process.env.SERVER_PORT;
 const app = express();
 const server = http.createServer(app);
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/users", userRouter);
-app.use("/weighIns", weighInsRouter);
-app.use("/workoutPlans", workoutPlanRouter);
-app.use("/presets/exercises", exercisePresetRouter);
-app.use("/workoutPlansPresets", workoutPlanPresetRouter);
-app.use("/presets/muscleGroups", muscleGroupPresetsRouter);
+app.use("/", router);
 
+app.use("*", (req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "An unexpected error occurred" });
+});
 
+if (!PORT) {
+  console.error("PORT environment variable not found");
+}
 
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 export default app;
