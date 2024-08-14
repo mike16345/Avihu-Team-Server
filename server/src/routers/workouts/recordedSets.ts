1a37c489fec37b56c70654ceaa712b052f57b843
mikeg16345 <michaelgani815@gmail.com>
2024-08-14 23:05:15 +0300

    moved files

--- server/src/routers/workouts/recordedSets.ts
+++ server/src/routers/workouts/recordedSets.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { RecordedSetsController } from "../controllers/recordedSetsController";
-import { validateRecordedSet } from "../middleware/recordedSetMiddleware";
+import { RecordedSetsController } from "../../controllers/recordedSetsController";
+import { validateRecordedSet } from "../../middleware/recordedSetMiddleware";
 
 const router = Router();
 
 router.post("/", validateRecordedSet, RecordedSetsController.addRecordedSet);
+
+router.post("/:sessionId", validateRecordedSet, RecordedSetsController.addRecordedSet);
 
 router.get("/user/:id", RecordedSetsController.getRecordedSetsByUserId);
 
