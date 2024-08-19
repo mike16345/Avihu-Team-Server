e35c8224dc45d94c5635237676ac55cc0703ef37
Noam Moskowitz <noammz101@gmail.com>
2024-08-19 11:47:48 +0300

    added prettier file and chaned phone number length

--- server/src/services/menuItemServices.ts
+++ server/src/services/menuItemServices.ts
@@ -1,68 +1,65 @@
 import { fullMenuItemPresets } from "../models/menuItemModel";
 
 export class MenuItemService {
- 
-    async addMenuItem(data: any,) {
-        try {
-            const newMenuItem = fullMenuItemPresets.create(data)
+  async addMenuItem(data: any) {
+    try {
+      const newMenuItem = fullMenuItemPresets.create(data);
 
-            return newMenuItem
-        } catch (error) {
-            throw error
-        }
+      return newMenuItem;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async getMenuItems(foodGroup: string) {
-        try {
-            const menuItems = await fullMenuItemPresets.find({ foodGroup: foodGroup })
+  async getMenuItems(foodGroup: string) {
+    try {
+      const menuItems = await fullMenuItemPresets.find({ foodGroup: foodGroup });
 
-            return menuItems
-        } catch (error) {
-            throw error
-        }
+      return menuItems;
+    } catch (error) {
+      throw error;
     }
-    async getOneMenuItem(id: string) {
-        try {
-            const menuItem = await fullMenuItemPresets.findOne({ _id: id })
+  }
+  async getOneMenuItem(id: string) {
+    try {
+      const menuItem = await fullMenuItemPresets.findOne({ _id: id });
 
-            return menuItem
-        } catch (error) {
-            throw error
-        }
+      return menuItem;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async getAllMenuItems() {
-        try {
-            const allMenuItems = await fullMenuItemPresets.find()
+  async getAllMenuItems() {
+    try {
+      const allMenuItems = await fullMenuItemPresets.find();
 
-            return allMenuItems
-        } catch (error) {
-            throw error
-        }
+      return allMenuItems;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async updateMenuItem(newMenuItem: any, id: string) {
-        try {
-            const updatedMenuItem = await fullMenuItemPresets.findOneAndUpdate(
-                { _id: id },
-                newMenuItem,
-                { new: true }
-            )
+  async updateMenuItem(newMenuItem: any, id: string) {
+    try {
+      const updatedMenuItem = await fullMenuItemPresets.findOneAndUpdate({ _id: id }, newMenuItem, {
+        new: true,
+      });
 
-            return updatedMenuItem
-        } catch (error) {
-            throw error
-        }
+      return updatedMenuItem;
+    } catch (error) {
+      throw error;
     }
-    async deleteMenuItem(id: string) {
-        try {
-            const deletedMenuItem = await fullMenuItemPresets.findOneAndRemove({ _id: id })
+  }
+  async deleteMenuItem(id: string) {
+    try {
+      const deletedMenuItem = await fullMenuItemPresets.findOneAndRemove({ _id: id });
 
-            return deletedMenuItem
-        } catch (error) {
-            throw error
-        }
+      return deletedMenuItem;
+    } catch (error) {
+      throw error;
     }
+  }
 }
 
-export const menuItemServices = new MenuItemService()
+export const menuItemServices = new MenuItemService();
