02189d9d82393b895bde568ab81e29b54b99d17a
mike16345 <95699872+mike16345@users.noreply.github.com>
2024-09-05 15:33:41 +0300

    removed sessions router

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import weighInsRouter from "./weighIns/";
 import presetsRouter from "./presets/";
-import sessionsRouter from "./sessions/";
 import analyticsRouter from "./analytics/MainAnalyticsRoutes";
 
 // Main router for all API endpoints
@@ -10,6 +9,5 @@ const router = express.Router();
 router.use("/analytics", analyticsRouter);
 router.use("/weighIns", weighInsRouter);
 router.use("/presets", presetsRouter);
-router.use("/sessions", sessionsRouter);
 
 export default router;
