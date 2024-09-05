6f88b1b523346bdf4a6e1569521a17c4093f8ff6
mike16345 <95699872+mike16345@users.noreply.github.com>
2024-09-05 18:35:21 +0300

    removed unused routers

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import weighInsRouter from "./weighIns/";
-import presetsRouter from "./presets/";
 import analyticsRouter from "./analytics/MainAnalyticsRoutes";
 
 // Main router for all API endpoints
@@ -8,6 +7,5 @@ const router = express.Router();
 
 router.use("/analytics", analyticsRouter);
 router.use("/weighIns", weighInsRouter);
-router.use("/presets", presetsRouter);
 
 export default router;
