675dbfaa7af55d98230d06afce0e5f30cd94244b
mike16345 <95699872+mike16345@users.noreply.github.com>
2024-09-05 13:25:34 +0300

    removed more routes

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -1,9 +1,7 @@
 import express from "express";
 import weighInsRouter from "./weighIns/";
-import dietPlanRouter from "./dietPlans";
 import presetsRouter from "./presets/";
 import sessionsRouter from "./sessions/";
-import workoutRouter from "./workouts";
 import analyticsRouter from "./analytics/MainAnalyticsRoutes";
 
 // Main router for all API endpoints
@@ -11,9 +9,7 @@ const router = express.Router();
 
 router.use("/analytics", analyticsRouter);
 router.use("/weighIns", weighInsRouter);
-router.use("/dietPlans", dietPlanRouter);
 router.use("/presets", presetsRouter);
 router.use("/sessions", sessionsRouter);
-router.use("/", workoutRouter);
 
 export default router;
--- server/src/routers/presets/index.ts
+++ server/src/routers/presets/index.ts
@@ -1,14 +1,10 @@
 import { Router } from "express";
 import workoutPlanPresetRouter from "./workoutPlanPresets";
-import menuItemRouter from "./menuItemPresets";
 import exercisePresetRouter from "./exercisePresets";
-import muscleGroupPresetsRouter from "./muscleGroupPresets";
 
 const presetsRouter = Router();
 
 presetsRouter.use("/workoutPlans", workoutPlanPresetRouter);
 presetsRouter.use("/exercises", exercisePresetRouter);
-presetsRouter.use("/muscleGroups", muscleGroupPresetsRouter);
-presetsRouter.use("/menuItems", menuItemRouter);
 
 export default presetsRouter;
