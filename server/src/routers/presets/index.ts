5036b40f7ebbe476f9080da226a021aee19763dd
mike16345 <95699872+mike16345@users.noreply.github.com>
2024-09-05 13:22:36 +0300

    removed unused routers

--- server/src/routers/presets/index.ts
+++ server/src/routers/presets/index.ts
@@ -3,7 +3,6 @@ import workoutPlanPresetRouter from "./workoutPlanPresets";
 import menuItemRouter from "./menuItemPresets";
 import exercisePresetRouter from "./exercisePresets";
 import muscleGroupPresetsRouter from "./muscleGroupPresets";
-import dietPlanPresetsRouter from "./dietPlanPresets";
 
 const presetsRouter = Router();
 
@@ -11,6 +10,5 @@ presetsRouter.use("/workoutPlans", workoutPlanPresetRouter);
 presetsRouter.use("/exercises", exercisePresetRouter);
 presetsRouter.use("/muscleGroups", muscleGroupPresetsRouter);
 presetsRouter.use("/menuItems", menuItemRouter);
-presetsRouter.use("/dietPlans", dietPlanPresetsRouter);
 
 export default presetsRouter;
--- server/src/routers/weighIns/index.ts
+++ server/src/routers/weighIns/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import weighInsRouter from "./weighIns";
 import weighInPhotosRouter from "./weighInPhotos";
 
 const router = Router();
 
-router.use("/weights", weighInsRouter);
 router.use("/photos", weighInPhotosRouter);
 
 export default router;
