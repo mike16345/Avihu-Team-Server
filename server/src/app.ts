1fc31ad80ba25a2ba87412607338c942a3f48d06
mike16345 <95699872+mike16345@users.noreply.github.com>
2024-09-10 18:54:15 +0300

    docker wip

--- server/src/app.ts
+++ server/src/app.ts
@@ -31,7 +31,7 @@ if (!PORT) {
 }
 
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
 
 export default app;
