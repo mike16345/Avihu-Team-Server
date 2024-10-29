8033c630285ff2bbfb7a46c94f2fd212f71a024b
mike16345 <95699872+mike16345@users.noreply.github.com>
2024-10-29 12:38:03 -0400

    created model for userImageUrls

---
+++ server/src/models/urlModel.ts
@@ -0,0 +1,27 @@
+import Joi from "joi";
+import { model, Schema } from "mongoose";
+
+export interface IUserImageUrls {
+  userId: string;
+  imageUrls: string[];
+}
+
+export const userImageUrlsSchema = new Schema<IUserImageUrls>({
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  imageUrls: {
+    type: [String],
+    required: true,
+    default: [],
+  },
+});
+
+export const UserImageUrlsModel = model("userImageUrls", userImageUrlsSchema);
+
+export const userImageSchemaValidator = Joi.object({
+  userId: Joi.string().required(),
+  imageUrl: Joi.string().min(1).required(),
+});
