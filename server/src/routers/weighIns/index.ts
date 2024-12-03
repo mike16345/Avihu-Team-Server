f1b57ebf8917bcd3370d8a54d3d5f291f6a3db85
mike16345 <95699872+mike16345@users.noreply.github.com>
2024-12-03 14:02:43 +0200

    deleted routers

--- server/src/routers/weighIns/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import weighInPhotosRouter from "./weighInPhotos";
-
-const router = Router();
-
-router.use("/photos", weighInPhotosRouter);
-
-export default router;
