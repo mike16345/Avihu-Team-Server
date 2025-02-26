09d84c70a76f2e7f195952eac4edc318d828e387
NoamMoskowitz <noammz101@gmail.com>
2025-02-26 21:15:31 +0200

    added exerciseMethod into cardio schema

--- server/src/models/workoutPlanModel.ts
+++ server/src/models/workoutPlanModel.ts
@@ -95,6 +95,7 @@ export const cardioWorkoutSchema = new Schema({
   distance: { type: String, required: true },
   cardioExercise: { type: String, required: true },
   tips: { type: String },
+  exerciseMethod: { type: String },
 });
 
 // Cardio Week Schema
@@ -170,6 +171,7 @@ export const cardioWorkoutValidationSchema = Joi.object({
   distance: Joi.string().required(),
   cardioExercise: Joi.string().required(),
   tips: Joi.string().allow(""),
+  exerciseMethod: Joi.string().allow(""),
 });
 
 export const cardioWeekValidationSchema = Joi.object({
