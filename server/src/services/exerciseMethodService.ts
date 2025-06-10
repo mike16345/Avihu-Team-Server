c24554918b2b7de60b064cf817abf4067d06925c
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-06-10 17:04:50 +0300

    formatted

--- server/src/services/exerciseMethodService.ts
+++ server/src/services/exerciseMethodService.ts
@@ -3,36 +3,35 @@ import { exerciseMethods } from "../models/excerciseMethodModel";
 import { PaginationParams } from "../utils/pagination";
 import { BaseService } from "./baseService";
 
+const RESOURCE_NAME = `exercise-methods`;
 
-
-const RESOURCE_NAME=`exercise-methods`
-
- class ExerciseMethodService extends BaseService<IExerciseMethod> {
-    async getAllExerciseMethods() {
-
+class ExerciseMethodService extends BaseService<IExerciseMethod> {
+  async getAllExerciseMethods() {
     try {
-      const allExerciseMethods =  await this.findAll(exerciseMethods,`${RESOURCE_NAME}-all`);
-      
+      const allExerciseMethods = await this.findAll(exerciseMethods, `${RESOURCE_NAME}-all`);
+
       return allExerciseMethods;
     } catch (error) {
       throw error;
     }
   }
 
-  async getPaginatedMethods(limit:number,page:number,query:any,sort:Record<string,any>){
+  async getPaginatedMethods(limit: number, page: number, query: any, sort: Record<string, any>) {
     try {
-      const paginatedData=await this.findPaginated({model:exerciseMethods,limit,page,sort,query}, RESOURCE_NAME)
+      const paginatedData = await this.findPaginated(
+        { model: exerciseMethods, limit, page, sort, query },
+        RESOURCE_NAME
+      );
 
-      return paginatedData
+      return paginatedData;
     } catch (error) {
-      throw error
+      throw error;
     }
   }
 
-   async getexErciseMethodById(id: string) {
-
+  async getexErciseMethodById(id: string) {
     try {
-      const exerciseMethod = await this.findById(id,exerciseMethods,`${RESOURCE_NAME}-${id}`)
+      const exerciseMethod = await this.findById(id, exerciseMethods, `${RESOURCE_NAME}-${id}`);
 
       return exerciseMethod;
     } catch (error) {
@@ -40,11 +39,13 @@ const RESOURCE_NAME=`exercise-methods`
     }
   }
 
-   async getExerciseMethodByName(name: string) {
-
+  async getExerciseMethodByName(name: string) {
     try {
-      const exerciseMethod =await this.findOne({title:name},exerciseMethods,`${RESOURCE_NAME}-${name}`)
-
+      const exerciseMethod = await this.findOne(
+        { title: name },
+        exerciseMethods,
+        `${RESOURCE_NAME}-${name}`
+      );
 
       return exerciseMethod;
     } catch (error) {
@@ -52,9 +53,9 @@ const RESOURCE_NAME=`exercise-methods`
     }
   }
 
-   async addExerciseMethod(exerciseMethod: IExerciseMethod) {
+  async addExerciseMethod(exerciseMethod: IExerciseMethod) {
     try {
-      const newExerciseMethod = await this.create(exerciseMethod,exerciseMethods)
+      const newExerciseMethod = await this.create(exerciseMethod, exerciseMethods);
 
       return newExerciseMethod;
     } catch (error) {
@@ -62,8 +63,7 @@ const RESOURCE_NAME=`exercise-methods`
     }
   }
 
-
-   async addManyExerciseMethods(exerciseMethods: IExerciseMethod[]) {
+  async addManyExerciseMethods(exerciseMethods: IExerciseMethod[]) {
     try {
       const newExrciseMethods = await Promise.all(
         exerciseMethods.map(async (e) => {
@@ -79,10 +79,9 @@ const RESOURCE_NAME=`exercise-methods`
     }
   }
 
-   async editExerciseMethod(exerciseMethod: IExerciseMethod, id: string) {
+  async editExerciseMethod(exerciseMethod: IExerciseMethod, id: string) {
     try {
-      const newExerciseMethod = await this.update(id,exerciseMethod,exerciseMethods)
-
+      const newExerciseMethod = await this.update(id, exerciseMethod, exerciseMethods);
 
       return newExerciseMethod;
     } catch (error) {
@@ -90,10 +89,9 @@ const RESOURCE_NAME=`exercise-methods`
     }
   }
 
-   async deleteExerciseMethod(id: string) {
+  async deleteExerciseMethod(id: string) {
     try {
-      const deletedExerciseMethod = await this.delete(id,exerciseMethods)
-
+      const deletedExerciseMethod = await this.delete(id, exerciseMethods);
 
       return deletedExerciseMethod;
     } catch (error) {
@@ -102,4 +100,4 @@ const RESOURCE_NAME=`exercise-methods`
   }
 }
 
-export default new ExerciseMethodService()
+export default new ExerciseMethodService();
