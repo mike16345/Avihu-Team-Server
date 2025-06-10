a3edda68b514b7b1f416ca7ebf5b03e06bcae589
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-06-10 20:33:33 +0300

    removed repository logic from service

--- server/src/services/baseService.ts
+++ server/src/services/baseService.ts
@@ -1,19 +1,20 @@
-import { FilterQuery, Model, UpdateWriteOpResult } from "mongoose";
+import { FilterQuery, UpdateWriteOpResult } from "mongoose";
 import { Cache } from "../utils/cache";
-import paginate, {
+import {
   generatePaginationCacheKey,
   PaginationParams,
   PaginationResult,
 } from "../utils/pagination";
 import { stableStringify } from "../utils/utils";
+import { BaseRepository } from "../repositories/BaseRepository";
 
 export class BaseService<T> {
   protected cache = new Cache<any>();
-  protected model: Model<T>;
+  protected repository: BaseRepository<T>;
   protected baseCacheKey: string;
 
-  constructor(model: Model<T>, baseCacheKey: string) {
-    this.model = model;
+  constructor(repository: BaseRepository<T>, baseCacheKey: string) {
+    this.repository = repository;
     this.baseCacheKey = baseCacheKey;
   }
 
@@ -21,25 +22,22 @@ export class BaseService<T> {
     return `${this.baseCacheKey}:${prefix}:${identifier}`;
   }
 
-  async create(doc: any): Promise<T> {
-    const newDoc = await this.model.create(doc);
-
-    if (!newDoc) throw new Error("Could not create item!");
-
+  async create(doc: any) {
+    const newDoc = await this.repository.create(doc);
     this.cache.invalidateAll();
 
     return newDoc;
   }
 
-  async find(query: FilterQuery<T> = {}, cacheKey?: string): Promise<T[]> {
-    const key = cacheKey || this.generateCacheKey("query", stableStringify(query));
+  async find(query: FilterQuery<T> = {}): Promise<T[]> {
+    const key = this.generateCacheKey("query", stableStringify(query));
     let data = this.cache.get(key);
 
-    if (!data) {
-      data = await this.model.find(query).lean().exec();
-      if (!data) throw new Error("Data could not be retrieved!");
-      this.cache.set(key, data);
-    }
+    if (data) return data;
+
+    data = await this.repository.find(query);
+    if (!data) throw new Error("Data could not be retrieved!");
+    this.cache.set(key, data);
 
     return data;
   }
@@ -50,102 +48,87 @@ export class BaseService<T> {
   ): Promise<PaginationResult<T>> {
     const cacheKey = this.generateCacheKey(
       "paginated",
-      generatePaginationCacheKey(resource || this.baseCacheKey, { model: this.model, ...query })
+      generatePaginationCacheKey(resource || this.baseCacheKey, query)
     );
+
     let data = this.cache.get(cacheKey);
 
-    if (!data) {
-      data = await paginate<T>({ model: this.model, ...query });
-      if (!data) throw new Error("Error retrieving page!");
-      this.cache.set(cacheKey, data);
-    }
+    if (data) return data;
+
+    data = await this.repository.getPaginated(query);
+    if (!data) throw new Error("Error retrieving page!");
+
+    this.cache.set(cacheKey, data);
 
     return data;
   }
 
-  async findById(id: string, cacheKey?: string): Promise<T | null> {
-    const key = cacheKey || this.generateCacheKey("id", id);
+  async findById(id: string) {
+    const key = this.generateCacheKey("id", id);
     let item = this.cache.get(key);
 
-    if (!item) {
-      item = await this.model.findById(id).lean().exec();
-      if (!item) throw new Error("Could not retrieve item!");
-      this.cache.set(key, item);
-    }
+    if (item) return item;
+
+    item = await this.repository.findById(id);
+    if (!item) throw new Error("Could not retrieve item!");
+    this.cache.set(key, item);
 
     return item;
   }
 
-  async findOne(query: FilterQuery<T>, cacheKey?: string): Promise<T | null> {
-    const key = cacheKey || this.generateCacheKey("one", stableStringify(query));
+  async findOne(query: FilterQuery<T>) {
+    const key = this.generateCacheKey("one", stableStringify(query));
     let item = this.cache.get(key);
 
-    if (!item) {
-      item = await this.model.findOne(query).lean().exec();
-      if (!item) throw new Error("Could not retrieve item!");
-      this.cache.set(key, item);
-    }
+    if (item) return item;
+
+    item = await this.repository.findOne(query);
+    if (!item) throw new Error("Could not retrieve item!");
+    this.cache.set(key, item);
 
     return item;
   }
 
   async updateOne(query: FilterQuery<T>, data: any) {
-    const updatedDoc = await this.model.findOneAndUpdate(query, data, { new: true }).lean().exec();
-
-    if (!updatedDoc) throw new Error("Item not found");
-
+    const updatedDoc = await this.repository.updateOne(query, data);
     this.cache.invalidateAll();
 
     return updatedDoc;
   }
 
   async updateById(id: string, data: any) {
-    const updatedDoc = await this.model.findByIdAndUpdate(id, data, { new: true }).lean().exec();
-
-    if (!updatedDoc) throw new Error("Item not found");
-
-    this.cache.invalidateAll();
+    const updatedDoc = await this.repository.updateById(id, data);
+    const key = this.generateCacheKey("id", id);
+    this.cache.invalidate(key);
 
     return updatedDoc;
   }
 
   async updateMany(query: FilterQuery<T>, data: any): Promise<UpdateWriteOpResult> {
-    const updateResult = await this.model.updateMany(query, data);
-
-    if (!updateResult) throw new Error("Update failed");
-
+    const result = await this.repository.updateMany(query, data);
     this.cache.invalidateAll();
 
-    return updateResult;
+    return result;
   }
 
   async deleteById(id: string) {
-    const deletedDoc = await this.model.findByIdAndDelete(id).lean().exec();
-
-    if (!deletedDoc) throw new Error("Item not found");
-
+    const deletedDoc = await this.repository.deleteById(id);
     this.cache.invalidateAll();
 
     return deletedDoc;
   }
 
   async delete(query: FilterQuery<T>) {
-    const deletedDoc = await this.model.findOneAndDelete(query).lean().exec();
-
-    if (!deletedDoc) throw new Error("Item not found");
-
+    const deletedDoc = await this.repository.delete(query);
     this.cache.invalidateAll();
 
     return deletedDoc;
   }
 
   async deleteMany(query: FilterQuery<T>): Promise<{ deletedCount?: number }> {
-    const deleteResult = await this.model.deleteMany(query);
-
-    if (!deleteResult) throw new Error("Delete failed");
-
+    const result = await this.repository.deleteMany(query);
     this.cache.invalidateAll();
 
-    return deleteResult;
+    return result;
   }
 }
