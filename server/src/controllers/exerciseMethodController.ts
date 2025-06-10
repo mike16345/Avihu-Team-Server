b45b33b796eff1ac8b2ad9a0cd219ac3ccb1b531
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-06-10 20:34:04 +0300

    created exercise method controller

--- server/src/controllers/exerciseMethodController.ts
+++ server/src/controllers/exerciseMethodController.ts
@@ -1,177 +1,34 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
-import { StatusCode } from "../enums/StatusCode";
-import { createResponse, createResponseWithData, createServerErrorResponse } from "../utils/utils";
-import  ExerciseMethodService  from "../services/exerciseMethodService";
+import ExerciseMethodService from "../services/exerciseMethodService";
+import BaseController from "./BaseController";
+import { IExerciseMethod } from "../interfaces/IWorkoutPlan";
 
-export default class ExerciseMethodController {
-  static async getAllExerciseMethods(
-    event: APIGatewayProxyEvent,
-    context: Context
-  ): Promise<APIGatewayProxyResult> {
-    try {
-      const allExerciseMethods = await ExerciseMethodService.getAllExerciseMethods();
-
-   
-
-      return createResponseWithData(
-        StatusCode.OK,
-        allExerciseMethods,
-        "Exercise methods retrieved successfully!"
-      );
-    } catch (error: any) {
-      return createServerErrorResponse(error.message);
-    }
+export default class ExerciseMethodController extends BaseController<IExerciseMethod> {
+  constructor() {
+    super(new ExerciseMethodService());
   }
 
-  static async getPaginatedExerciseMethods(
-    event: APIGatewayProxyEvent,
-    context: Context
-  ): Promise<APIGatewayProxyResult> {
-    const { limit,page, query={}, sort } = event.queryStringParameters || {};
-
-    try {
-      const paginatedExerciseMethods = await ExerciseMethodService.getPaginatedMethods(limit,page,query,sort);
-
-    
-      return createResponseWithData(
-        StatusCode.OK,
-        paginatedExerciseMethods,
-        "Exercise methods retrieved successfully!"
-      );
-    } catch (error: any) {
-      return createServerErrorResponse(error.message);
-    }
+  create(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    return super.create(event);
   }
 
-  static async getExerciseMethodById(
-    event: APIGatewayProxyEvent,
-    context: Context
-  ): Promise<APIGatewayProxyResult> {
-    const { id } = event.queryStringParameters || {};
-
-    try {
-      const exerciseMethod = await ExerciseMethodService.getexErciseMethodById(id || "");
-
-    
-
-      return createResponseWithData(
-        StatusCode.OK,
-        exerciseMethod,
-        "Exercise method retrieved successfully!"
-      );
-    } catch (error: any) {
-      return createServerErrorResponse(error.message);
-    }
-  }
-  static async getExerciseMethodByName(
-    event: APIGatewayProxyEvent,
-    context: Context
-  ): Promise<APIGatewayProxyResult> {
-    const { name } = event.queryStringParameters || {};
-
-    try {
-      const exerciseMethod = await ExerciseMethodService.getExerciseMethodByName(name || "");
-
-    
-
-      return createResponseWithData(
-        StatusCode.OK,
-        exerciseMethod,
-        "Exercise method retrieved successfully!"
-      );
-    } catch (error: any) {
-      return createServerErrorResponse(error.message);
-    }
+  getAll(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    return super.getAll(event);
   }
 
-  static async addManyExerciseMethod(
-    event: APIGatewayProxyEvent,
-    context: Context
-  ): Promise<APIGatewayProxyResult> {
-    const exerciseMethods = JSON.parse(event.body || "{}");
-
-    try {
-      const newExerciseMethods = await ExerciseMethodService.addManyExerciseMethods(
-        exerciseMethods
-      );
-
-      if (!newExerciseMethods) {
-        return createResponse(StatusCode.BAD_REQUEST, "לא הצלחנו ליצור את שיטות האימון!");
-      }
-
-      return createResponseWithData(
-        StatusCode.CREATED,
-        newExerciseMethods,
-        "Exercise method added successfully!"
-      );
-    } catch (error: any) {
-      return createServerErrorResponse(error.message);
-    }
-  }
-  static async addExerciseMethod(
-    event: APIGatewayProxyEvent,
-    context: Context
-  ): Promise<APIGatewayProxyResult> {
-    const exerciseMethod = JSON.parse(event.body || "{}");
-
-    try {
-      const newExerciseMethod = await ExerciseMethodService.addExerciseMethod(exerciseMethod);
-
-    
-
-      return createResponseWithData(
-        StatusCode.CREATED,
-        newExerciseMethod,
-        "Exercise method added successfully!"
-      );
-    } catch (error: any) {
-      return createServerErrorResponse(error.message);
-    }
+  getOne(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    return this.getOne(event);
   }
 
-  static async editExerciseMethod(
-    event: APIGatewayProxyEvent,
-    context: Context
-  ): Promise<APIGatewayProxyResult> {
-    const exerciseMethod = JSON.parse(event.body || "{}");
-    const { id } = event.queryStringParameters || {};
-
-    try {
-      const updatedExerciseMethod = await ExerciseMethodService.editExerciseMethod(
-        exerciseMethod,
-        id || ""
-      );
-
-    
-
-      return createResponseWithData(
-        StatusCode.OK,
-        updatedExerciseMethod,
-        "Muscle group updated successfully!"
-      );
-    } catch (error: any) {
-      return createServerErrorResponse(error.message);
-    }
+  getById(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    return super.getById(event);
   }
 
-  static async deleteExerciseMethod(
-    event: APIGatewayProxyEvent,
-    context: Context
-  ): Promise<APIGatewayProxyResult> {
-    const { id } = event.queryStringParameters || {};
+  updateById(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    return super.updateById(event);
+  }
 
-    try {
-      const deletedExerciseMethod = await ExerciseMethodService.deleteExerciseMethod(id || "");
-
-     
-
-      return createResponseWithData(
-        StatusCode.OK,
-        deletedExerciseMethod,
-        "Exercise method deleted successfully!"
-      );
-    } catch (error: any) {
-      return createServerErrorResponse(error.message);
-    }
+  deleteById(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    return super.deleteById(event);
   }
 }
