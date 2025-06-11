7d2277f4160147c555d293c4f3fb170dc747c3a0
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-06-11 15:21:06 +0300

    clean jups

--- server/src/repositories/BaseRepository.ts
+++ server/src/repositories/BaseRepository.ts
@@ -1,5 +1,5 @@
 import { FilterQuery, Model, UpdateWriteOpResult } from "mongoose";
-import paginate, { PaginationParams, PaginationResult } from "../utils/pagination";
+import { PaginationParams, PaginationResult } from "../utils/pagination";
 
 export class BaseRepository<T> {
   protected model: Model<T>;
@@ -10,12 +10,15 @@ export class BaseRepository<T> {
 
   async create(doc: any): Promise<T> {
     const newDoc = await this.model.create(doc);
+
     if (!newDoc) throw new Error("Could not create item!");
+
     return newDoc;
   }
 
   async find(query: FilterQuery<T> = {}) {
     const data = await this.model.find(query).lean().exec();
+
     if (!data) throw new Error("Data could not be retrieved!");
 
     return data;
@@ -23,6 +26,7 @@ export class BaseRepository<T> {
 
   async findById(id: string) {
     const item = await this.model.findById(id).lean().exec();
+
     if (!item) throw new Error("Could not retrieve item!");
 
     return item;
@@ -30,7 +34,9 @@ export class BaseRepository<T> {
 
   async findOne(query: FilterQuery<T>) {
     const item = await this.model.findOne(query).lean().exec();
+
     if (!item) throw new Error("Could not retrieve item!");
+
     return item;
   }
 
@@ -61,37 +67,49 @@ export class BaseRepository<T> {
 
   async updateOne(query: FilterQuery<T>, data: any) {
     const updatedDoc = await this.model.findOneAndUpdate(query, data, { new: true }).lean().exec();
+
     if (!updatedDoc) throw new Error("Item not found");
+
     return updatedDoc;
   }
 
   async updateById(id: string, data: any) {
     const updatedDoc = await this.model.findByIdAndUpdate(id, data, { new: true }).lean().exec();
+
     if (!updatedDoc) throw new Error("Item not found");
+
     return updatedDoc;
   }
 
   async updateMany(query: FilterQuery<T>, data: any): Promise<UpdateWriteOpResult> {
     const updateResult = await this.model.updateMany(query, data);
+
     if (!updateResult) throw new Error("Update failed");
+
     return updateResult;
   }
 
   async deleteById(id: string) {
     const deletedDoc = await this.model.findByIdAndDelete(id).lean().exec();
+
     if (!deletedDoc) throw new Error("Item not found");
+
     return deletedDoc;
   }
 
   async delete(query: FilterQuery<T>) {
     const deletedDoc = await this.model.findOneAndDelete(query).lean().exec();
+
     if (!deletedDoc) throw new Error("Item not found");
+
     return deletedDoc;
   }
 
   async deleteMany(query: FilterQuery<T>): Promise<{ deletedCount?: number }> {
     const deleteResult = await this.model.deleteMany(query);
+
     if (!deleteResult) throw new Error("Delete failed");
+
     return deleteResult;
   }
 }
