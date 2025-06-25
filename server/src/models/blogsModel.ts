013c254c585c452314b0c438e17e0988d012cd93
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-06-25 17:01:34 +0300

    removed interface

--- server/src/models/blogsModel.ts
+++ server/src/models/blogsModel.ts
@@ -2,11 +2,6 @@ import Joi from "joi";
 import { model, Schema } from "mongoose";
 import { IBlog } from "../interfaces/IBlog";
 
-export interface IUserImageUrls {
-  userId: string;
-  imageUrls: string[];
-}
-
 export const blogSchema = new Schema<IBlog>({
   title: {
     type: String,
