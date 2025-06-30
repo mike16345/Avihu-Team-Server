f60d5f5681dbd81e6d1608d5eaa8e87770eea872
NoamMoskowitz <noammz101@gmail.com>
2025-06-30 18:02:28 +0300

    refactored diet plan service

--- server/src/services/dietPlanService.ts
+++ server/src/services/dietPlanService.ts
@@ -1,66 +1,51 @@
-import { DietPlan } from "../models/dietPlanModel";
-import { fullMenuItemPresets } from "../models/menuItemModel";
-import { Cache } from "../utils/cache";
+import { FilterQuery } from "mongoose";
+import { IDietPlan } from "../interfaces/IDietPlan";
+import { DietPlanRepository } from "../repositories/DietPlan/DietPlanRepository";
+import { stableStringify } from "../utils/utils";
+import { BaseService } from "./BaseService";
 
-let cachedDietPlans = new Cache<any>();
-const getDietPlan = async (filter: { _id?: string; userId?: string }, populate: boolean = true) => {
-  if (!filter._id && !filter.userId) throw new Error("Missing required id or userId parameter");
 
-  const cacheKey = filter._id || filter.userId;
-  const cached = cachedDietPlans.get(cacheKey! + populate.valueOf());
 
-  try {
-    const query = DietPlan.findOne(filter).select({ _id: false, __v: false });
 
-    if (populate) {
-      query
-        .populate({
-          path: "meals.totalProtein.customItems",
-          model: fullMenuItemPresets,
-        })
-        .populate({
-          path: "meals.totalCarbs.customItems",
-          model: fullMenuItemPresets,
-        });
-    }
+const baseKey='diet-plan'; 
 
-    const dietPlan = cached || (await query.lean());
-    cachedDietPlans.set(cacheKey!, dietPlan);
-
-    return dietPlan;
-  } catch (err: any) {
-    throw err;
-  }
-};
-
-export class DietPlanService {
-  async addDietPlan(data: any) {
-    try {
-      const dietPlanDoc = await DietPlan.create(data);
-      cachedDietPlans.invalidateAll();
-
-      return dietPlanDoc;
-    } catch (err: any) {
-      throw err;
-    }
+export class DietPlanService extends BaseService<IDietPlan,DietPlanRepository> {
+  constructor(){
+    super(new DietPlanRepository(),baseKey)
   }
 
-  async getAllDietPlans() {
-    const cached = cachedDietPlans.get("all");
 
-    try {
-      const dietPlans = cached || (await DietPlan.find({}));
-      cachedDietPlans.set("all", dietPlans);
+  getDietPlan=async(query:FilterQuery<IDietPlan>,populate:boolean)=>{
 
-      return dietPlans;
-    } catch (err: any) {
-      throw err;
-    }
+    if(populate){
+      const key= this.generateCacheKey('one',stableStringify({...query,populate}))
+  
+       const cached=this.cache.get(key);
+  
+       if(cached) return cached;
+  
+       const dietPlan=await this.repository.getPopulatedDietPlan(query);
+  
+       this.cache.set(key,dietPlan)
+  
+       return dietPlan
+      }else{
+  
+        
+        return await this.findOne(query)
+      }
   }
+  
 
-  async getDietPlanById(planId: string, populate: boolean = true) {
+
+
+   getDietPlanById=async( planId: string, populate: boolean = true)=> {
+    const query={_id:planId};
+    
     try {
-      return getDietPlan({ _id: planId }, populate);
+
+   return await this.getDietPlan(query,populate);
+  
     } catch (error) {
       throw error;
     }
@@ -68,79 +53,19 @@ export class DietPlanService {
 
   async getDietPlanByUserId(userId: string, populate: boolean = true) {
     try {
-      return getDietPlan({ userId }, populate);
+      return await this.getDietPlan({ userId }, populate);
     } catch (error) {
       throw error;
     }
   }
 
-  async deleteDietPlan(planId: string) {
-    try {
-      const deletedDietPlan = await DietPlan.findByIdAndDelete(planId);
-      if (deletedDietPlan) {
-        cachedDietPlans.invalidate("all");
-        cachedDietPlans.invalidate(deletedDietPlan.userId);
-        cachedDietPlans.invalidate(String(deletedDietPlan._id));
-      }
+ 
 
-      return deletedDietPlan || `Diet plan not found for ID: ${planId}`;
-    } catch (err: any) {
-      throw err;
-    }
-  }
+ 
 
-  async deleteDietPlanByUserId(userId: string) {
-    try {
-      const deletedDietPlan = await DietPlan.findOneAndDelete({ userId: userId });
+ 
 
-      if (!deletedDietPlan) {
-        return `Diet plan not found/deleted for user ID: ${userId}`;
-      }
-      cachedDietPlans.invalidate("all");
-      cachedDietPlans.invalidate(deletedDietPlan.userId);
-      cachedDietPlans.invalidate(String(deletedDietPlan._id));
-
-      return deletedDietPlan;
-    } catch (err: any) {
-      throw err;
-    }
-  }
-
-  async updateDietPlanByUserId(userId: string, dietPlan: any) {
-    try {
-      const updatedDietPlan = await DietPlan.findOneAndUpdate({ userId }, dietPlan, {
-        new: true,
-      });
-
-      if (updatedDietPlan) {
-        cachedDietPlans.invalidate("all");
-        cachedDietPlans.invalidate(userId);
-        cachedDietPlans.invalidate(String(updatedDietPlan._id));
-      }
-
-      return updatedDietPlan;
-    } catch (err: any) {
-      throw err;
-    }
-  }
-
-  async updateDietPlan(dietPlanId: string, newDietPlan: any) {
-    try {
-      const updatedDietPlan = await DietPlan.findByIdAndUpdate(dietPlanId, newDietPlan, {
-        new: true,
-      });
-
-      if (updatedDietPlan) {
-        cachedDietPlans.invalidate("all");
-        cachedDietPlans.invalidate(updatedDietPlan.userId);
-        cachedDietPlans.invalidate(dietPlanId);
-      }
-
-      return updatedDietPlan;
-    } catch (err: any) {
-      throw err;
-    }
-  }
+  
 }
 
-export const DietPlanServices = new DietPlanService();
+
