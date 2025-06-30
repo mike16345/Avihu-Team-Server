c2d7c6ebc089c996225a9e6f30bcfd510721b8f8
mike16345 <mike16345@users.noreply.github.com>
2025-06-30 10:19:37 +0000

    chore: auto-format with Prettier

--- server/src/unformatted-file.ts
+++ server/src/unformatted-file.ts
@@ -1,27 +1,26 @@
-const s = "this is not formateed at all";
+const s = 'this is not formateed at all';
 
 function unformattedFunction() {
-  console.log("This function is not formatted either");
+      console.log('This function is not formatted either');
 }
 
 const d = {
-  key1: "value1",
+      key1: 'value1',
 };
 
-const newd = "this is a new variable";
+const newd = 'this is a new variable';
 const anotherUnformattedVariable = {
-  key2: "value2",
-  key3: "value3",
-    
-}
+      key2: 'value2',
+      key3: 'value3',
+};
 
 const unformattedObject = {
-  key1: "value1",
-  key2: "value2",   }
+      key1: 'value1',
+      key2: 'value2',
+};
 
+const str = 's';
 
-const str = 's'
+const str2 = 's2';
 
-const str2 = "s2"
-
-const unformattedArray = [1, 2, 3, 4, 5, "4", "5"];
+const unformattedArray = [1, 2, 3, 4, 5, '4', '5'];
