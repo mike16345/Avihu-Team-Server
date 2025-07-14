119086f72cdd08aacdafe0e5292b67dab8b09e0b
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-07-14 18:37:50 +0300

    refactor: clean up error handling in BaseRepository and enhance SessionRepository with getSessionById method

--- server/src/repositories/Sessions/SessionRepository.ts
+++ server/src/repositories/Sessions/SessionRepository.ts
@@ -1,8 +1,14 @@
+import { Document, Types } from "mongoose";
 import Session, { ISession } from "../../models/sessionModel";
+import { FindOptionsNoQuery } from "../../types/mongooseTypes";
 import { BaseRepository } from "../BaseRepository";
 
 export class SessionRepository extends BaseRepository<ISession> {
   constructor() {
     super(Session);
+  }
+
+  async getSessionById(id: string, options?: FindOptionsNoQuery<ISession> | undefined) {
+    return await this.model.findById(id, options);
   }
 }
