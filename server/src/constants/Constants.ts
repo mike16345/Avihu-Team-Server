63bf5d17bfd82e620469e09cd659e1d7d40da660
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-08-18 11:55:13 +0300

    format code

--- server/src/constants/Constants.ts
+++ server/src/constants/Constants.ts
@@ -4,9 +4,9 @@ export const ONE_MINUTE_IN_MILLISECONDS = 60000;
 export const HALF_DAY_IN_MILLISECONDS = 60 * 12 * ONE_MINUTE_IN_MILLISECONDS;
 export const ONE_WEEK_IN_SECONDS = 7 * 24 * 60 * 60; // 7 days in seconds
 
-export const AVG_PROTEIN_CALORIES=150
-export const AVG_FAT_CALORIES=100
-export const AVG_CARB_CALORIES=120
+export const AVG_PROTEIN_CALORIES = 150;
+export const AVG_FAT_CALORIES = 100;
+export const AVG_CARB_CALORIES = 120;
 
 export const DUPLICATE_PRESET_ERROR = "שם התבנית כבר קיים במערכת!";
 
