7ed9df04223ad83a1757873b93f9f129be06258c
Noam Moskowitz <noammz101@gmail.com>
2025-09-11 20:27:16 +0300

    created index function

---
+++ server/src/functions/MuscleMeasurements/index.ts
@@ -0,0 +1,20 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { handleApiCall } from "../baseHandler";
+import MuscleMeasurementController from "../../controllers/MuscleMeasurementController";
+
+const BASE_PATH = "/muscleMeasurements";
+
+const muscleMeasurementController = new MuscleMeasurementController();
+
+const muscleMeasurementsApiHandlers = {
+  [`GET ${BASE_PATH}/one`]: muscleMeasurementController.getMeasurementsByUserId,
+  [`POST ${BASE_PATH}`]: muscleMeasurementController.saveMeasurement,
+  [`DELETE ${BASE_PATH}`]: muscleMeasurementController.removeMeasurement,
+};
+
+export const handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  return await handleApiCall(event, context, muscleMeasurementsApiHandlers);
+};
