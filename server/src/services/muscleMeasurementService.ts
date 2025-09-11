febfeb181154158c61c59d4a26ca0af33bc10ac5
Noam Moskowitz <noammz101@gmail.com>
2025-09-11 20:20:16 +0300

    created service

---
+++ server/src/services/muscleMeasurementService.ts
@@ -0,0 +1,61 @@
+import { BaseService } from "./BaseService";
+import { MuscleMeasurementsRepository } from "../repositories/MuscleMeasurments/MuscleMeasurmentsRepository";
+import { IUserMuscleMeasurements } from "../interfaces/IMuscleMeasurment";
+
+const RESOURCE_NAME = "muscle-measurement";
+
+export class muscleMeasurementService extends BaseService<
+  IUserMuscleMeasurements,
+  MuscleMeasurementsRepository
+> {
+  constructor() {
+    super(new MuscleMeasurementsRepository(), RESOURCE_NAME);
+  }
+
+  async saveMeasurment(userId: string, date: string, measurement: number, muscle: string) {
+    try {
+      const muscleMeasurement = await this.repository.saveMeasurment(
+        userId,
+        date,
+        measurement,
+        muscle
+      );
+
+      this.cache.invalidateAllContaining(userId);
+
+      return muscleMeasurement;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getUsersMuscleMeasurements(userId: string) {
+    const cached = this.cache.get(userId);
+
+    if (cached) return;
+
+    try {
+      const measurementRecord: IUserMuscleMeasurements = await this.repository.findOne({
+        query: { userId },
+      });
+
+      this.cache.set(userId, measurementRecord);
+
+      return measurementRecord;
+    } catch (err: any) {
+      throw err;
+    }
+  }
+
+  async removeMeasurement(userId: string, date: string, muscle: string) {
+    try {
+      const deletedProgressNote = this.repository.removeMeasurement(userId, date, muscle);
+
+      this.cache.invalidateAllContaining(userId);
+
+      return deletedProgressNote;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
