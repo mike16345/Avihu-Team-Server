f03f19c7b5b2f9621b2b1b323fb93214ab8d18b9
Noam Moskowitz <noammz101@gmail.com>
2025-09-29 20:33:50 +0300

    added parsing of query object received in getPaginated method

--- server/src/repositories/BaseRepository.ts
+++ server/src/repositories/BaseRepository.ts
@@ -29,7 +29,7 @@ export class BaseRepository<T> {
 
     return count !== null;
   }
-  
+
   async find(options: FindOptions<T> = { query: {} }) {
     const { query, projection, queryOptions } = options;
     const data = await this.model.find(query, projection, queryOptions);
@@ -71,10 +71,11 @@ export class BaseRepository<T> {
     sort = {},
   }: PaginationParams): Promise<PaginationResult<T>> {
     const skip = (page - 1) * limit;
+    const parsedQuery = JSON.parse(query);
 
     const [results, totalResults] = await Promise.all([
-      this.model.find(query).sort(sort).skip(skip).limit(limit),
-      this.model.countDocuments(query).exec(),
+      this.model.find(parsedQuery).sort(sort).skip(skip).limit(limit),
+      this.model.countDocuments(parsedQuery).exec(),
     ]);
 
     const totalPages = Math.ceil(totalResults / limit);
