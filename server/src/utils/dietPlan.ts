f492f9f78269de5ee3438bd72ff1dcee60135682
Noam Moskowitz <noammz101@gmail.com>
2025-10-22 15:16:55 +0300

    removed calculation of fat calories

--- server/src/utils/dietPlan.ts
+++ server/src/utils/dietPlan.ts
@@ -7,7 +7,6 @@ export const calculateTotalCalories = (meals: IMeal[], totalFats = 0) => {
 
   const proteinCalories = totalProteins * AVG_PROTEIN_CALORIES;
   const carbCalories = totalCarbs * AVG_CARB_CALORIES;
-  const fatCalories = totalFats * AVG_FAT_CALORIES;
 
-  return proteinCalories + carbCalories + fatCalories;
+  return proteinCalories + carbCalories;
 };
