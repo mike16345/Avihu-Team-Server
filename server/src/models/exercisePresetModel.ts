5feb8ac071b8ab7719ec19ff68e928ebf81386a7
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-11-05 16:15:11 +0200

    allow tip from trainer in exercise preset

--- server/src/models/exercisePresetModel.ts
+++ server/src/models/exercisePresetModel.ts
@@ -21,6 +21,10 @@ export const exercisePresetSchema = new Schema<IExercisePreset>({
   imageUrl: {
     type: String,
   },
+  tipFromTrainer:{
+    type:String,
+    required:false,
+  }
 });
 
 export const exercisePresets = model(`exercisePresets`, exercisePresetSchema);
@@ -32,4 +36,6 @@ export const exercisePresetValidationSchema = Joi.object({
   linkToVideo: Joi.string().min(27).pattern(youtubeUrlPattern).required(),
   muscleGroup: Joi.string().min(1).required(),
   imageUrl: Joi.string().allow(""),
+  tipFromTrainer:Joi.string().allow(""),
+
 });
