aa3dffea302653123551e6d91808d169582069d7
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-11-13 16:42:55 +0200

    feat: add Leads function to deployAllLambdas and update API path handling

--- server/src/functions/Leads/index.ts
+++ server/src/functions/Leads/index.ts
@@ -10,14 +10,14 @@ const leadsController = new LeadsController();
 const leadsApiHandlers = {
   [`POST ${BASE_PATH}`]: leadsController.create,
   [`GET ${BASE_PATH}`]: leadsController.list,
-  [`GET ${BASE_PATH}/{id}`]: leadsController.getById,
-  [`PUT ${BASE_PATH}/{id}`]: leadsController.update,
-  [`DELETE ${BASE_PATH}/{id}`]: leadsController.remove,
+  [`GET ${BASE_PATH}/one`]: leadsController.getById,
+  [`PUT ${BASE_PATH}/one`]: leadsController.update,
+  [`DELETE ${BASE_PATH}/one`]: leadsController.remove,
 };
 
 const leadsApiValidators = {
   [`POST ${BASE_PATH}`]: validateCreateLead,
-  [`PUT ${BASE_PATH}/{id}`]: validateUpdateLead,
+  [`PUT ${BASE_PATH}/one`]: validateUpdateLead,
 };
 
 const normalizeEventPath = (event: APIGatewayProxyEvent): APIGatewayProxyEvent => {
