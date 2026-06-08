2f508c659ab5bcac10f5d3252552a5702271e996
mike16345 <mike16345@users.noreply.github.com>
2026-06-08 15:24:44 +0000

    chore: auto-format with Prettier

--- server/src/interfaces/IWorkoutPlan.ts
+++ server/src/interfaces/IWorkoutPlan.ts
@@ -51,12 +51,7 @@ export type WorkoutGoal =
   | "endurance"
   | "toning"
   | "rehab";
-export type WorkoutEquipment =
-  | "gym"
-  | "studio"
-  | "weights"
-  | "bodyweight"
-  | "weights-bodyweight";
+export type WorkoutEquipment = "gym" | "studio" | "weights" | "bodyweight" | "weights-bodyweight";
 
 export interface IWorkoutPlanMeta {
   workoutsPerWeek?: number;
